6a48c50ad1256bdd5b5bbd82679f4b5b4c7f2170
weilandr0843 <weilandr0843@ATC15601>
2016-11-08 11:08:36 -0500

    Added the Person table and PersonDataSet.

--- Tut11.1-11.4_WeilandR/Tut11.1-11.4_WeilandR/Form1.cs
+++ Tut11.1-11.4_WeilandR/Tut11.1-11.4_WeilandR/Form1.cs
@@ -16,5 +16,20 @@ namespace Tut11._1_11._4_WeilandR
         {
             InitializeComponent();
         }
+
+        private void personBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.personBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.personDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'personDataSet.Person' table. You can move, or remove it, as needed.
+            this.personTableAdapter.Fill(this.personDataSet.Person);
+
+        }
     }
 }

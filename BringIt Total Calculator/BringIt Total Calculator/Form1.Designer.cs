1e5ee36464cfc4b3061413a3ac326a6590860a1f
weilandr0843 <weilandr0843@ATC104B20>
2017-01-28 09:30:27 -0800

    I have begun work on the code. By using my old assignment for the Phonebook, I was able to determine how to read a file. Now if I figure out how to multiply an indexed item and a double, I might be able to finish this assignment.

--- BringIt Total Calculator/BringIt Total Calculator/Form1.Designer.cs
+++ BringIt Total Calculator/BringIt Total Calculator/Form1.Designer.cs
@@ -28,9 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.costPrompt = new System.Windows.Forms.Label();
-            this.costTextBox = new System.Windows.Forms.TextBox();
-            this.nameTextBox = new System.Windows.Forms.TextBox();
             this.namePrompt = new System.Windows.Forms.Label();
             this.countyLabelPrompt = new System.Windows.Forms.Label();
             this.stateLabelPrompt = new System.Windows.Forms.Label();
@@ -43,49 +40,28 @@
             this.tipsPrompt = new System.Windows.Forms.Label();
             this.tipsTextBox = new System.Windows.Forms.TextBox();
             this.typePrompt = new System.Windows.Forms.Label();
-            this.calculateButton = new System.Windows.Forms.Button();
             this.itemTypeLabel = new System.Windows.Forms.Label();
+            this.ordersListBox = new System.Windows.Forms.ListBox();
+            this.tipButton = new System.Windows.Forms.Button();
+            this.itemCostLabel = new System.Windows.Forms.Label();
+            this.costLabelPrompt = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // costPrompt
-            // 
-            this.costPrompt.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
-            this.costPrompt.AutoSize = true;
-            this.costPrompt.Location = new System.Drawing.Point(12, 33);
-            this.costPrompt.Name = "costPrompt";
-            this.costPrompt.Size = new System.Drawing.Size(63, 13);
-            this.costPrompt.TabIndex = 0;
-            this.costPrompt.Text = "Cost of Item";
-            // 
-            // costTextBox
-            // 
-            this.costTextBox.Location = new System.Drawing.Point(95, 30);
-            this.costTextBox.Name = "costTextBox";
-            this.costTextBox.Size = new System.Drawing.Size(100, 20);
-            this.costTextBox.TabIndex = 2;
-            // 
-            // nameTextBox
-            // 
-            this.nameTextBox.Location = new System.Drawing.Point(95, 6);
-            this.nameTextBox.Name = "nameTextBox";
-            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
-            this.nameTextBox.TabIndex = 1;
             // 
             // namePrompt
             // 
             this.namePrompt.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.namePrompt.AutoSize = true;
-            this.namePrompt.Location = new System.Drawing.Point(12, 9);
+            this.namePrompt.Location = new System.Drawing.Point(68, 9);
             this.namePrompt.Name = "namePrompt";
-            this.namePrompt.Size = new System.Drawing.Size(70, 13);
+            this.namePrompt.Size = new System.Drawing.Size(81, 13);
             this.namePrompt.TabIndex = 3;
-            this.namePrompt.Text = "Name of Item";
+            this.namePrompt.Text = "Select an Order";
             // 
             // countyLabelPrompt
             // 
             this.countyLabelPrompt.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.countyLabelPrompt.AutoSize = true;
-            this.countyLabelPrompt.Location = new System.Drawing.Point(14, 150);
+            this.countyLabelPrompt.Location = new System.Drawing.Point(16, 205);
             this.countyLabelPrompt.Name = "countyLabelPrompt";
             this.countyLabelPrompt.Size = new System.Drawing.Size(61, 13);
             this.countyLabelPrompt.TabIndex = 6;
@@ -95,7 +71,7 @@
             // 
             this.stateLabelPrompt.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.stateLabelPrompt.AutoSize = true;
-            this.stateLabelPrompt.Location = new System.Drawing.Point(14, 173);
+            this.stateLabelPrompt.Location = new System.Drawing.Point(16, 228);
             this.stateLabelPrompt.Name = "stateLabelPrompt";
             this.stateLabelPrompt.Size = new System.Drawing.Size(53, 13);
             this.stateLabelPrompt.TabIndex = 5;
@@ -105,7 +81,7 @@
             // 
             this.deliveryLabelPrompt.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.deliveryLabelPrompt.AutoSize = true;
-            this.deliveryLabelPrompt.Location = new System.Drawing.Point(14, 196);
+            this.deliveryLabelPrompt.Location = new System.Drawing.Point(16, 251);
             this.deliveryLabelPrompt.Name = "deliveryLabelPrompt";
             this.deliveryLabelPrompt.Size = new System.Drawing.Size(79, 13);
             this.deliveryLabelPrompt.TabIndex = 8;
@@ -115,7 +91,7 @@
             // 
             this.totalLabelPrompt.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.totalLabelPrompt.AutoSize = true;
-            this.totalLabelPrompt.Location = new System.Drawing.Point(14, 219);
+            this.totalLabelPrompt.Location = new System.Drawing.Point(16, 274);
             this.totalLabelPrompt.Name = "totalLabelPrompt";
             this.totalLabelPrompt.Size = new System.Drawing.Size(31, 13);
             this.totalLabelPrompt.TabIndex = 7;
@@ -125,43 +101,47 @@
             // 
             this.countyLabel.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.countyLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.countyLabel.Location = new System.Drawing.Point(97, 149);
+            this.countyLabel.Location = new System.Drawing.Point(97, 201);
             this.countyLabel.Name = "countyLabel";
             this.countyLabel.Size = new System.Drawing.Size(100, 20);
             this.countyLabel.TabIndex = 6;
+            this.countyLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // stateLabel
             // 
             this.stateLabel.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.stateLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.stateLabel.Location = new System.Drawing.Point(97, 172);
+            this.stateLabel.Location = new System.Drawing.Point(97, 224);
             this.stateLabel.Name = "stateLabel";
             this.stateLabel.Size = new System.Drawing.Size(100, 20);
             this.stateLabel.TabIndex = 6;
+            this.stateLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // deliveryLabel
             // 
             this.deliveryLabel.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.deliveryLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.deliveryLabel.Location = new System.Drawing.Point(97, 195);
+            this.deliveryLabel.Location = new System.Drawing.Point(97, 247);
             this.deliveryLabel.Name = "deliveryLabel";
             this.deliveryLabel.Size = new System.Drawing.Size(100, 20);
             this.deliveryLabel.TabIndex = 6;
+            this.deliveryLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // totalLabel
             // 
             this.totalLabel.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.totalLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.totalLabel.Location = new System.Drawing.Point(97, 218);
+            this.totalLabel.Location = new System.Drawing.Point(97, 270);
             this.totalLabel.Name = "totalLabel";
             this.totalLabel.Size = new System.Drawing.Size(100, 20);
             this.totalLabel.TabIndex = 6;
+            this.totalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // tipsPrompt
             // 
             this.tipsPrompt.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.tipsPrompt.AutoSize = true;
-            this.tipsPrompt.Location = new System.Drawing.Point(12, 57);
+            this.tipsPrompt.Location = new System.Drawing.Point(16, 106);
             this.tipsPrompt.Name = "tipsPrompt";
             this.tipsPrompt.Size = new System.Drawing.Size(27, 13);
             this.tipsPrompt.TabIndex = 0;
@@ -169,7 +149,7 @@
             // 
             // tipsTextBox
             // 
-            this.tipsTextBox.Location = new System.Drawing.Point(95, 54);
+            this.tipsTextBox.Location = new System.Drawing.Point(97, 103);
             this.tipsTextBox.Name = "tipsTextBox";
             this.tipsTextBox.Size = new System.Drawing.Size(100, 20);
             this.tipsTextBox.TabIndex = 3;
@@ -178,65 +158,90 @@
             // 
             this.typePrompt.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.typePrompt.AutoSize = true;
-            this.typePrompt.Location = new System.Drawing.Point(12, 84);
+            this.typePrompt.Location = new System.Drawing.Point(16, 159);
             this.typePrompt.Name = "typePrompt";
             this.typePrompt.Size = new System.Drawing.Size(54, 13);
             this.typePrompt.TabIndex = 9;
             this.typePrompt.Text = "Item Type";
             // 
-            // calculateButton
-            // 
-            this.calculateButton.Location = new System.Drawing.Point(15, 110);
-            this.calculateButton.Name = "calculateButton";
-            this.calculateButton.Size = new System.Drawing.Size(198, 23);
-            this.calculateButton.TabIndex = 10;
-            this.calculateButton.Text = "Calculate";
-            this.calculateButton.UseVisualStyleBackColor = true;
-            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
-            // 
             // itemTypeLabel
             // 
             this.itemTypeLabel.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.itemTypeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.itemTypeLabel.Location = new System.Drawing.Point(95, 83);
+            this.itemTypeLabel.Location = new System.Drawing.Point(97, 155);
             this.itemTypeLabel.Name = "itemTypeLabel";
             this.itemTypeLabel.Size = new System.Drawing.Size(100, 20);
             this.itemTypeLabel.TabIndex = 6;
+            this.itemTypeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // ordersListBox
+            // 
+            this.ordersListBox.FormattingEnabled = true;
+            this.ordersListBox.Location = new System.Drawing.Point(19, 25);
+            this.ordersListBox.Name = "ordersListBox";
+            this.ordersListBox.Size = new System.Drawing.Size(178, 69);
+            this.ordersListBox.TabIndex = 10;
+            // 
+            // tipButton
+            // 
+            this.tipButton.Location = new System.Drawing.Point(19, 129);
+            this.tipButton.Name = "tipButton";
+            this.tipButton.Size = new System.Drawing.Size(178, 23);
+            this.tipButton.TabIndex = 11;
+            this.tipButton.Text = "Add Tip";
+            this.tipButton.UseVisualStyleBackColor = true;
+            // 
+            // itemCostLabel
+            // 
+            this.itemCostLabel.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.itemCostLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.itemCostLabel.Location = new System.Drawing.Point(97, 178);
+            this.itemCostLabel.Name = "itemCostLabel";
+            this.itemCostLabel.Size = new System.Drawing.Size(100, 20);
+            this.itemCostLabel.TabIndex = 6;
+            this.itemCostLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // costLabelPrompt
+            // 
+            this.costLabelPrompt.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.costLabelPrompt.AutoSize = true;
+            this.costLabelPrompt.Location = new System.Drawing.Point(16, 182);
+            this.costLabelPrompt.Name = "costLabelPrompt";
+            this.costLabelPrompt.Size = new System.Drawing.Size(51, 13);
+            this.costLabelPrompt.TabIndex = 9;
+            this.costLabelPrompt.Text = "Item Cost";
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(225, 242);
-            this.Controls.Add(this.calculateButton);
+            this.ClientSize = new System.Drawing.Size(216, 301);
+            this.Controls.Add(this.tipButton);
+            this.Controls.Add(this.ordersListBox);
+            this.Controls.Add(this.costLabelPrompt);
             this.Controls.Add(this.typePrompt);
             this.Controls.Add(this.deliveryLabelPrompt);
             this.Controls.Add(this.totalLabelPrompt);
             this.Controls.Add(this.totalLabel);
             this.Controls.Add(this.deliveryLabel);
+            this.Controls.Add(this.itemCostLabel);
             this.Controls.Add(this.stateLabel);
             this.Controls.Add(this.itemTypeLabel);
             this.Controls.Add(this.countyLabel);
             this.Controls.Add(this.countyLabelPrompt);
             this.Controls.Add(this.stateLabelPrompt);
-            this.Controls.Add(this.nameTextBox);
             this.Controls.Add(this.namePrompt);
             this.Controls.Add(this.tipsTextBox);
-            this.Controls.Add(this.costTextBox);
             this.Controls.Add(this.tipsPrompt);
-            this.Controls.Add(this.costPrompt);
             this.Name = "Main";
             this.Text = "BringIt";
+            this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label costPrompt;
-        private System.Windows.Forms.TextBox costTextBox;
-        private System.Windows.Forms.TextBox nameTextBox;
         private System.Windows.Forms.Label namePrompt;
         private System.Windows.Forms.Label countyLabelPrompt;
         private System.Windows.Forms.Label stateLabelPrompt;
@@ -249,8 +254,11 @@
         private System.Windows.Forms.Label tipsPrompt;
         private System.Windows.Forms.TextBox tipsTextBox;
         private System.Windows.Forms.Label typePrompt;
-        private System.Windows.Forms.Button calculateButton;
         private System.Windows.Forms.Label itemTypeLabel;
+        private System.Windows.Forms.ListBox ordersListBox;
+        private System.Windows.Forms.Button tipButton;
+        private System.Windows.Forms.Label itemCostLabel;
+        private System.Windows.Forms.Label costLabelPrompt;
     }
 }
 
--- BringIt Total Calculator/BringIt Total Calculator/Form1.cs
+++ BringIt Total Calculator/BringIt Total Calculator/Form1.cs
@@ -11,38 +11,101 @@ using System.IO;
 
 namespace BringIt_Total_Calculator
 {
+    struct OrderEntry
+    {
+        public string name;
+        public string type;
+        public string cost;
+    }
     public partial class Main : Form
     {
+        //Field to hold a list of orders.
+        private List<OrderEntry> orderList =
+            new List<OrderEntry>();
         public Main()
         {
             InitializeComponent();
         }
-        private void calculateButton_Click(object sender, EventArgs e)
+
+        //The ReadFile method reads of the contents of the
+        //Orders.txt file and stores it as Orders
+        //objects in the Orders list.
+        private void ReadFile()
         {
-            //Variables
-            string itemName = "";
-            double itemCost = 0;
-            double itemTips = 0;
-            string itemType = "";
-
-            //Read a text file for the orders.
-            System.IO.StringReader.Equals;
-            
-
-
-            //Determine whether or not the itemCost is under $10.
-            if
+            try
             {
+                StreamReader inputFile; //To read the file.
+                string line;            //To hold a line from the file.
 
+                //Create an instance of the PhoneBookEntry structure.
+                OrderEntry entry = new OrderEntry();
+
+                //Create a delimiter array.
+                char[] delim = { ' ' };
+
+                //Open the PhoneList file.
+                inputFile = File.OpenText("Orders.txt");
+
+                //Read the lines from the file.
+                while (!inputFile.EndOfStream)
+                {
+                    //Read a line from the file.
+                    line = inputFile.ReadLine();
+
+                    //Tokenize the line
+                    string[] tokens = line.Split(delim);
+
+                    //Store the tokens in an entry object.
+                    entry.name = tokens[0];
+                    entry.type = tokens[1];
+                    entry.cost = tokens[2];
+
+                    //Add the entry object to the List.
+                    orderList.Add(entry);
+                }
             }
-            
-            //Calculate Tax accordingly and add the BringIt Delivery Charges.
+            catch (Exception ex)
+            {
+                //Display an error message.
+                MessageBox.Show(ex.Message);
+            }
+        }
+        //The DisplayNames method displays the list of names
+        //in the namesListBox control.
+        private void DisplayNames()
+        {
+            foreach (OrderEntry entry in orderList)
+            {
+                ordersListBox.Items.Add(entry.name);
+            }
+        }
+        private void Main_Load(object sender, EventArgs e)
+        {
+            //Read the PhoneList.txt file.
+            ReadFile();
 
-            //Calculate everything together.
+            //Display the names.
+            DisplayNames();
+        }
 
-            //Display the results.
+        private void nameListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            decimal itemCost = 0;
+            double countyRate = .0225;
+            double stateRate = .0475;
 
+            //Get the index of the selected item.
+            int index = ordersListBox.SelectedIndex;
 
+            //Display the corrosponding type.
+            itemTypeLabel.Text = orderList[index].type;
+
+            //Display the corrosponding cost.
+            itemCostLabel.Text = orderList[index].cost;
+            itemCost = orderList[index].cost;
+
+            //Display the corrosponding county tax.
+            countyLabel.Text = (orderList[index].cost * countyRate);
         }
     }
 }
